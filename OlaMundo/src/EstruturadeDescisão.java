196bab2988908aa207511e6c8ef3fbf7b4b05adc
caio_lelis <caio_lelis@samsung-caio>
2022-11-03 19:22:14 -0300

    Att aula 02 Fabiana

--- OlaMundo/src/EstruturadeDescisão.java
+++ OlaMundo/src/EstruturadeDescisão.java
@@ -1,9 +1,23 @@
-
+import java.util.Scanner;
 public class EstruturadeDescisão {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+		int n1, n2;
+		Scanner entrada = new Scanner (System.in);
+		System.out.println("Digite um número inteiro:");
+		n1 = entrada.nextInt();
+		System.out.println("Digite outro número inteiro:");
+		n2 = entrada.nextInt();
+		
+		if (n1 > n2) {
+			System.out.println(n1 + " é maior que" + n2);
+			} else {
+				if (n2 > n1)
+					System.out.println(n2 + " é maior que " + n1);
+				else 
+				System.out.println("Os números são iguais");
+			
+			}
+	}	
 }
