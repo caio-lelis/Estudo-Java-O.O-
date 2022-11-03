40fcdc820cd03aec63c2cec74ba46e0c268886de
caio_lelis <caio_lelis@samsung-caio>
2022-11-03 19:40:11 -0300

    Att aula 02 Fabiana

---
+++ OlaMundo/src/EstruturadeDecisãoSwitch.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class EstruturadeDecisãoSwitch {
+
+	public static void main(String[] args) {
+		int num;
+		Scanner entrada = new Scanner (System.in);
+		System.out.println("Digite um número de 1 a 3");
+		num = entrada.nextInt();
+		
+		switch(num ) {
+		case 1:
+			System.out.println("Você digitou 1");
+			break;
+		case 2:
+			System.out.println("você digitou 2");
+			break;
+		case 3:
+			System.out.println("você digitou 3");
+			break;
+		default:
+			System.out.println("Você não digitou 1, 2 ou 3");
+			break;
+		}
+	}
+
+}
